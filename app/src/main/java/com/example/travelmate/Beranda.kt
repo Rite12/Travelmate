cf9915deab1007e51941e54242cc46c8f0d7597a
Rite12 <irgimuhammad6@gmail.com>
2024-06-03 22:26:47 +0700

    version 1.0.5 adding homepage, promo fragment_promo.xml, and fragment_beranda.xml

--- app/src/main/java/com/example/travelmate/Beranda.kt
+++ app/src/main/java/com/example/travelmate/Beranda.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.travelmate.R
 
 class HomeFragment : Fragment() {
@@ -14,4 +16,24 @@ class HomeFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_beranda, container, false)
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val promoBtn: ImageView = view.findViewById(R.id.promo_image)
+
+        promoBtn.setOnClickListener {
+            val promoFragment = PromoFragment()
+            replaceFragment(promoFragment)
+        }
+
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        parentFragmentManager.beginTransaction()
+        .replace(R.id.fragment_container, fragment)
+        .addToBackStack(null)
+        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
+        .commit()
+        }
 }
